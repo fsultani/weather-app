977e21afc587b1f580e4462645da2043bb590add
Farid Sultani <faridsultani.ba@gmail.com>
2019-10-01 18:55:21 -0700

    Catch errors for erroneous zip codes

--- src/actions/fetchWeather.js
+++ src/actions/fetchWeather.js
@@ -4,12 +4,23 @@ const fetchWeather = zip => dispatch => {
   const API_KEY = 'fa8426178454105aa9e5a7f3ae72353a';
   fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}&units=imperial`)
   .then(res => res.json())
-  .then(payload => dispatch({
-    type: FETCH_WEATHER,
-    payload
-  }))
+  .then(payload => {
+    if (payload.cod === 200) {
+      return dispatch({
+        type: FETCH_WEATHER,
+        payload
+      })
+    } else if (payload.cod === '404') {
+      alert(`${zip}: ${payload.message}.  Click "OK" to reload the page and try a different zip code.`);
+      window.location.reload();
+    } else {
+      alert("There was an error. Click 'OK' to reload the page.");
+      window.location.reload();
+    }
+  })
   .catch(error => {
     alert("There was an error");
+    window.location.reload();
     console.log("error\n", error);
   })
 }
